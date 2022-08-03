b809886f69f6df7c4ab93c988cda1e8ed3cb164e
迷渡 <justjavac@gmail.com>
2022-08-03 21:32:53 +0800

    Revert "remove gfm (#13)"
    This reverts commit fb0581513885f41e8ea59fad906c9582543ed05e.

--- main.ts
+++ main.ts
@@ -1,9 +1,47 @@
 import { serve } from "https://deno.land/std@0.149.0/http/server.ts";
 import * as echarts from "https://cdn.jsdelivr.net/npm/echarts/dist/echarts.esm.js";
+import { CSS, render } from "https://deno.land/x/gfm@0.1.22/mod.ts";
 
-serve((request) => {
+serve(async (request) => {
   const searchParams = new URL(request.url).searchParams;
-  const option = searchParams.get("option") ?? "{}";
+  const option = searchParams.get("option");
+
+  if (option == null) {
+    const readme = await Deno.readTextFile("./README.md");
+    const body = render(readme);
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Chart Service</title>
+        <style>
+          body {
+            margin: 0;
+            background-color: var(--color-canvas-default);
+            color: var(--color-fg-default);
+          }
+          main {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 2rem 1rem;
+          }
+          ${CSS}
+        </style>
+      </head>
+      <body data-color-mode="auto" data-light-theme="light" data-dark-theme="dark">
+        <main class="markdown-body">
+          ${body}
+        </main>
+      </body>
+    </html>`;
+    return new Response(html, {
+      headers: {
+        "content-type": "text/html;charset=utf-8",
+      },
+    });
+  }
+
   const width = parseInt(searchParams.get("w") ?? "500");
   const height = parseInt(searchParams.get("h") ?? "300");
 
